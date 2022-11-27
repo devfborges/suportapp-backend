3ffca4a2701086d4ce5f9dc036f76d7d7995f2a4
Felipe Borges <joaofelipeborgescruz@outlook.com>
2022-11-27 10:57:43 -0300

    Add called model

---
+++ src/models/called.js
@@ -0,0 +1,27 @@
+const mongoose = require('../config/db.connect')
+
+const calledSchema = new mongoose.Schema({
+
+    clinica: {
+        type: String,
+        require: true,
+    },
+    suport: {
+        type: String,
+        require: true,
+        enum: ['João Pedro', 'Fernando', 'Ítalo', 'João Felipe']
+    },
+    description: {
+        type: String,
+        require: true,
+    },
+    dateCreated: {
+        type: Date,
+        default: Date.now,
+    }
+
+})
+
+const Called = mongoose.model('Called', calledSchema)
+
+module.exports = Called
